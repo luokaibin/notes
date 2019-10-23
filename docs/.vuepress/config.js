8b2f27b6784544b799f73b782a25166e8f3ffd8e
胖子 <luokb@ebaiyihui.com>
2019-10-23 16:23:06 +0800

    删除打包文件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,9 +7,15 @@ module.exports = {
     updatePopup: true,
   }],
   head: [
-    ['link', { rel: 'icon', href: 'logo.ico' }],
-    //增加manifest.json
+    ['link', { rel: 'icon', href: 'logo.png' }],
     ['link', { rel: 'manifest', href: 'manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: 'logo.png' }],
+    ['link', { rel: 'mask-icon', href: 'logo.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: 'logo_144*144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
   ],
   themeConfig: {
     sidebar: [
