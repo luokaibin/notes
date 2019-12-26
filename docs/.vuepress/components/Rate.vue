9caf7e2090df94a8be7cbfd1c1adaca92b1098e4
胖子 <luokb@ebaiyihui.com>
2019-12-26 18:22:23 +0800

    update

---
+++ docs/.vuepress/components/Rate.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <span class="item" v-for="i of count" :key="i">&#9733;</span>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    count: {
+      type: Number,
+      default: 5,
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+.container {
+  display: inline-block;
+}
+.container .item {
+  font-size: 20px;
+  letter-spacing: 0.1em;
+  color: #F1F100;
+}
+</style>
